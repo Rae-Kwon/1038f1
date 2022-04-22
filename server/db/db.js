8bc5ff3b8a1f6cfb0ab209c68e2be536f64011a5
Richard Kwon <rkminjae@gmail.com>
2022-04-22 13:10:06 -0400

    add database connection

--- server/db/db.js
+++ server/db/db.js
@@ -1,7 +1,21 @@
 const Sequelize = require("sequelize");
 
-const db = new Sequelize(process.env.DATABASE_URL || "postgres://localhost:5432/messenger", {
-  logging: false
-});
+const db = new Sequelize(
+	process.env.DATABASE_URL || "postgres://localhost:5432/messenger",
+	{
+		logging: false,
+	}
+);
+
+const connectToDb = async () => {
+	try {
+		await db.authenticate();
+		console.log("Connection to database successful");
+	} catch (error) {
+		console.error("Unable to connect to the database", error.message);
+	}
+};
+
+connectToDb()
 
 module.exports = db;
