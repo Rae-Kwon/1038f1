abd992a78812ceffc506c2ac1f69faea0a4fd6aa
Richard Kwon <rkminjae@gmail.com>
2022-05-03 18:12:08 -0400

    refactor with john's feedback

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -1,23 +1,19 @@
 const Conversation = require("./conversation");
 const User = require("./user");
-const UserConversations = require('./userConversations')
+const UserConversations = require("./userConversations");
 const Message = require("./message");
-
-// associations
-
-User.hasMany(Conversation)
-Conversation.belongsTo(User, { as: "user1" });
-Conversation.belongsTo(User, { as: "user2" });
-Message.belongsTo(Conversation);
-Conversation.hasMany(Message);
 
 // Many-to-many relationship between users table and conversations table
 User.belongsToMany(Conversation, { through: UserConversations, as: "users" });
 Conversation.belongsToMany(User, { through: UserConversations });
 
+User.hasMany(Conversation);
+Message.belongsTo(Conversation);
+Conversation.hasMany(Message);
+
 module.exports = {
-  User,
-  Conversation,
-  Message,
-  UserConversations
+	User,
+	Conversation,
+	Message,
+	UserConversations,
 };
